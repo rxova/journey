b2ab1b8058bcaee02e22c7917f3bcd995de85b14
Jonatan Kruszewski <jonatankruszewski@gmail.com>
2026-03-14 19:16:29 +0200

    feat(docs): ts

---
+++ apps/docs/src/theme/MDXComponents.ts
@@ -0,0 +1,11 @@
+import MDXComponents from "@theme-original/MDXComponents";
+import TabItem from "@theme/TabItem";
+import Tabs from "@theme/Tabs";
+
+const components = {
+  ...MDXComponents,
+  Tabs,
+  TabItem
+};
+
+export default components;
